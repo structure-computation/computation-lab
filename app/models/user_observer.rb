43f318e3bee51a2c69b8f0aff96dda32bf69c2c2
Raphael Bellec <raphael@macbook-pro-de-raphael.local>
2010-01-12 23:10:02 +0100

    Creation des modeles et config de Restfull Auth. Ajout des utilisateurs, session, routes, correction du bug potentiel d'activation, ajout de l'observer dans la configuration de l'environnement, ajout de la migration (sans corrections encore).
    Reste a ajouter :
    Les modifications de la migration
    L'auth par email et non par login
    Les relations
    La configuration du mail
    Les vues

---
+++ app/models/user_observer.rb
@@ -0,0 +1,16 @@
+class UserObserver < ActiveRecord::Observer
+  def after_create(user)
+    
+    # Correction du bug (frequent mais non systematique) de restfull auth. Le mauvais code d'activation etait envoye.
+    user.reload
+    UserMailer.deliver_signup_notification(user)
+  end
+
+  def after_save(user)
+
+    # Correction du bug (frequent mais non systematique) de restfull auth. Le mauvais code d'activation etait envoye.
+    user.reload  
+    UserMailer.deliver_activation(user) if user.recently_activated?
+  
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 ActionController::Routing::Routes.draw do |map|
+  
+  # Routes nommees pour restful authentication.
+  map.logout    '/logout',    :controller => 'sessions', :action => 'destroy'
+  map.login     '/login',     :controller => 'sessions', :action => 'new'
+  map.register  '/register',  :controller => 'users', :action => 'create'
+  map.signup    '/signup',    :controller => 'users', :action => 'new'
+  
+  # Ressources User et session (auth)
+  map.resources :users
+  map.resource  :session
+
+  # Route d'activation des utilisateurs.
+  map.activate ‘/activate/:activation_code’, :controller => ‘users’, :action => ‘activate’, :activation_code => nil
+
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
