ab49f8a1fc1638c1f43f7163c0d3b3fb5dd7c4c3
Raphael Bellec <r.bellec@structure-computation.com>
2010-01-16 23:44:26 +0100

    Ajout des relations dans le modele User Modification de présentation des fixtures.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,37 @@
 require 'digest/sha1'
 
 class User < ActiveRecord::Base
+  # Inclusions, nottament pour Restfull Auth.
   include Authentication
   include Authentication::ByPassword
   include Authentication::ByCookieToken
   include  Authorization::AasmRoles
 
-  validates_presence_of     :login
-  validates_length_of       :login,    :within => 3..40
-  validates_uniqueness_of   :login
-  validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
+  
+  # Relations
+  belongs_to :company
+  
+  
+  has_many    :sc_models
+  has_many    :user_projects,                                             :dependent => :destroy # Pour les gestionnaires, reattribuer ce projet.
+  has_many    :projects,       :through => :user_projects
+  has_many    :owned_projects, :through => :user_projects, :source => :task , :conditions => { "user_projects.is_admin"     => true }
+  
+  # Gestion des tâches. 2 types de tâches + toutes les tâches : 3 relations + la relation de jointure.
+  has_many    :user_tasks
+  has_many    :tasks         , :through => :user_tasks,                   :dependent => :destroy
+  has_many    :created_tasks , :through => :user_tasks, :source => :task    , :conditions => { "user_tasks.is_creator"      => true }
+  has_many    :assigned_tasks, :through => :user_tasks, :source => :task    , :conditions => { "user_tasks.is_assigned_to"  => true }
+
+  has_many    :log_calculs
+  
+  
+  # Validations
+
+  # validates_presence_of     :login
+  # validates_length_of       :login,    :within => 3..40
+  # validates_uniqueness_of   :login
+  # validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
 
   validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
   validates_length_of       :name,     :maximum => 100
@@ -24,7 +46,7 @@ class User < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation
+  attr_accessible  :email, :name, :password, :password_confirmation
 
 
 
@@ -34,15 +56,12 @@ class User < ActiveRecord::Base
   # We really need a Dispatch Chain here or something.
   # This will also let us return a human error message.
   #
-  def self.authenticate(login, password)
-    return nil if login.blank? || password.blank?
-    u = find_in_state :first, :active, :conditions => {:login => login.downcase} # need to get the salt
+  def self.authenticate(email, password)
+    return nil if email.blank? || password.blank?
+    u = find_in_state :first, :active, :conditions => {:email => email.downcase} # need to get the salt
     u && u.authenticated?(password) ? u : nil
   end
 
-  def login=(value)
-    write_attribute :login, (value ? value.downcase : nil)
-  end
 
   def email=(value)
     write_attribute :email, (value ? value.downcase : nil)
--- db/migrate/20091222163139_create_projects.rb
+++ db/migrate/20091222163139_create_projects.rb
@@ -1,13 +1,13 @@
 class CreateProjects < ActiveRecord::Migration
   def self.up
-    create_table :projects do |t|
-      t.string :name
-      t.text :description
-      t.date :start_date
-      t.date :estimated_end_date
-      t.date :end_date
-      t.integer :estimated_done
-      t.string :state
+    create_table  :projects do |t|
+      t.string    :name
+      t.text      :description
+      t.date      :start_date
+      t.date      :estimated_end_date
+      t.date      :end_date
+      t.integer   :estimated_done
+      t.string    :state
 
       t.timestamps
     end
