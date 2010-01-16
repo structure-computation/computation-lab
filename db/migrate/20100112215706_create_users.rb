bfd9f4e85542de26e26502b8328eaf6923b3809d
Raphael Bellec <r.bellec@structure-computation.com>
2010-01-16 23:43:44 +0100

    Definition des données d'un User.

--- db/migrate/20100112215706_create_users.rb
+++ db/migrate/20100112215706_create_users.rb
@@ -1,9 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table "users", :force => true do |t|
-      t.column :login,                     :string, :limit => 40
-      t.column :name,                      :string, :limit => 100, :default => '', :null => true
+      # t.column :login,                     :string, :limit => 40
+      # Relations
+      t.column :company_id,                      :integer
+      
+      t.column :firstname,                      :string, :limit => 100, :default => '', :null => true
+      t.column :lastname,                       :string, :limit => 100, :default => '', :null => true
+      t.column :telephone,                      :string, :limit => 23 , :default => '', :null => true # Souvenirs des normes ISUP etc, le max devait etre 15, lu 22 recemment...
       t.column :email,                     :string, :limit => 100
+      
+      # restfull auth
       t.column :crypted_password,          :string, :limit => 40
       t.column :salt,                      :string, :limit => 40
       t.column :created_at,                :datetime
@@ -15,7 +22,7 @@ class CreateUsers < ActiveRecord::Migration
       t.column :state,                     :string, :null => :no, :default => 'passive'
       t.column :deleted_at,                :datetime
     end
-    add_index :users, :login, :unique => true
+    add_index :users, :email, :unique => true
   end
 
   def self.down
