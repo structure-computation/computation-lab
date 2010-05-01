45d274d10858abe709d1dca992832d5bde1d1852
Raphaël Bellec <raphael@macbook-pro-de-raphael.local>
2010-05-01 22:39:12 +0200

    Squashed commit of the following:
    commit ec6f2c8d0c61f9eded64ad176ac7110d6f547146
    Author: Raphaël Bellec <raphael@macbook-pro-de-raphael.local>
    Date:   Sat Feb 20 00:41:27 2010 +0100
    
        Ajout des premieres fixtures.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
   
   # Routes nommees pour restful authentication.
-  map.logout    '/logout',    :controller => 'sessions', :action => 'destroy'
-  map.login     '/login',     :controller => 'sessions', :action => 'new'
-  map.register  '/register',  :controller => 'users', :action => 'create'
-  map.signup    '/signup',    :controller => 'users', :action => 'new'
+  map.logout    '/logout',    :controller => 'sessions' , :action => 'destroy'
+  map.login     '/login',     :controller => 'sessions' , :action => 'new'
+  map.register  '/register',  :controller => 'users'    , :action => 'create'
+  map.signup    '/signup',    :controller => 'users'    , :action => 'new'
   
   # Ressources User et session (auth)
   map.resources :users
