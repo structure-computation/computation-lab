af7d4e847cecc7bb7e7459f87329168ebf364f4c
Raphaël Bellec <contact@bellec.in>
2011-03-15 13:59:25 +0100

    Configuration du mailer (de dev seulement) pour devise.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,9 @@ SCInterface::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  
+  # TODO: Il faut configurer le HOST de action_mailer
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' } 
 
   # Enable threaded mode
   # config.threadsafe!
