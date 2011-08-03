24381b82eb5d04ccd2fc99af0c9051c7ec70fbec
Nima Izadi <nim.izadi@gmail.com>
2011-08-03 09:51:57 +0200

    Modification des vues Materiels
    Dans la vu "new", les checkboxes sont à la ligne est aligné.
    Les égales dans les tableaux des vus "show" sont dans des TD et aligné à droite.

--- app/controllers/sc_models_controller.rb
+++ app/controllers/sc_models_controller.rb
@@ -6,7 +6,8 @@ class ScModelsController < InheritedResources::Base
   before_filter :authenticate_user!
   before_filter :set_page_name
   belongs_to :company
-
+  respond_to :html, :json
+  
   layout 'company'
 #  belongs_to :member
   
