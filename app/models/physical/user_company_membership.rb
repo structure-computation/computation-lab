7b1530ab4196bcc6a9fa0a82d3c4fd481e09760c
Raphaël Bellec <contact@bellec.in>
2011-08-09 13:38:05 +0200

    Mise à jour du schéma. Ajout d'un exemple commenté dans user_company_membership.rb

--- app/models/physical/user_company_membership.rb
+++ app/models/physical/user_company_membership.rb
@@ -5,4 +5,14 @@ class UserCompanyMembership < ActiveRecord::Base
   has_many    :model_ownerships, :class_name => "UserModelOwnership", :foreign_key => "company_member_id"
   has_many    :sc_models,                 :through => :model_ownerships
 
+  # TODO: exemple pour mise en place des scopes.
+  # scope :manager, where(Member.manager_condition_sql)
+  # 
+  # def self.manager_condition_sql
+  #   " members.role LIKE '%manager%' OR  members.role LIKE '%all%' "
+  # end
+  # 
+
+
+
 end
