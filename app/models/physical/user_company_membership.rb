b84a87b8185df438bc8157cf29f8486b8fca0262
Raphaël Bellec <contact@bellec.in>
2011-08-11 00:54:17 +0200

    Erreur dans la définition d'une relation has many Corrigée. Confusion entre l'ancienne table (effacée depuis) et la nouvelle methode multi tenant.

--- app/models/physical/user_company_membership.rb
+++ app/models/physical/user_company_membership.rb
@@ -2,7 +2,7 @@ class UserCompanyMembership < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :company
   
-  has_many    :model_ownerships, :class_name => "UserModelOwnership", :foreign_key => "company_member_id"
+  has_many    :model_ownerships, :class_name => "CompanyMemberToModelOwnership", :foreign_key => "company_member_id"
   has_many    :sc_models,                 :through => :model_ownerships
 
   # TODO: exemple pour mise en place des scopes.
