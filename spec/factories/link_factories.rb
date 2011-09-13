e667c9736826d40c762e271aba27c04df230c6f2
Raphaël Bellec <contact@bellec.in>
2011-09-13 12:50:25 +0200

    Rspec controlleurs, premiers test Ok avec FactoryGirl. Après de nombreux essais il a été impossible d'utiliser les stubs pour redéfinir des methodes de classe sur les objets active record. Exemple : Link.stub(:all => ...)
    Nous avons fait le choix d'utiliser factory girl qui fonctionne après
    quelques essais.

---
+++ spec/factories/link_factories.rb
@@ -0,0 +1,41 @@
+FactoryGirl.define do
+  sequence :link_name do |n|
+    "link_#{n}"
+  end
+  sequence :material_name do |n|
+    "material_#{n}"
+  end
+  
+  factory :link, :class => Link do 
+    name "demo link" #{link_name}
+    
+    factory :standard_link do
+      workspace_id -1
+    end
+    
+  end
+  
+end
+
+
+# Définition de link
+# t.string   "name"
+# t.string   "family"
+# t.integer  "workspace_id"
+# t.integer  "reference"
+# t.integer  "id_select"
+# t.string   "name_select"
+# t.text     "description"
+# t.string   "comp_generique"
+# t.string   "comp_complexe"
+# t.integer  "type_num"
+# t.float    "Ep"
+# t.float    "jeu"
+# t.float    "R"
+# t.float    "f"
+# t.float    "Lp"
+# t.float    "Dp"
+# t.float    "p"
+# t.float    "Lr"
+# t.datetime "created_at"
+# t.datetime "updated_at"
