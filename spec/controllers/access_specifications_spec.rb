c4bf0b62afbf1cae4e1ae94facd26da5031225b5
stephanie chhim <stephaniechhim@gmail.com>
2011-09-15 18:57:06 +0200

    Suite travail sur test du controller de Workspace en fonction du rôle des Users

--- spec/controllers/access_specifications_spec.rb
+++ spec/controllers/access_specifications_spec.rb
@@ -8,36 +8,36 @@ describe "Access rights on RESTressources" do
     let :other_workspace          do FactoryGirl.build(:workspace)        end                                     
     let :user                     do FactoryGirl.build(:user)             end                                     
     #let :mock_workspace_member  do mock_model(UserWorkspaceMembership, :workspace => current_workspace ).as_null_object end    
-    let :mock_workspace_engineer  do mock_model(UserWorkspaceMembership, :workspace => current_workspace, 
-                                                                         :role => engineer ).as_null_object 
-                                  end
-    let :mock_workspace_manager   do mock_model(UserWorkspaceMembership, :workspace => current_workspace, 
-                                                                         :role => manager ).as_null_object 
-                                  end                              
-                                  
-      
-    before(:each) do
-      controller.stub(:authenticate_user!         => true                     )                 
-      controller.stub(:current_workspace_engineer => mock_workspace_engineer  )
-      controller.stub(:current_workspace_manager  => mock_workspace_manager   ) 
-      controller.stub(:current_workspace          => current_workspace        )
-      current_workspace.save
-      other_workspace.save
-    end  
+    # let :mock_workspace_engineer  do mock_model(UserWorkspaceMembership, :workspace => current_workspace, 
+    #                                                                         :role => engineer ).as_null_object 
+    #                                  end
+    #    let :mock_workspace_manager   do mock_model(UserWorkspaceMembership, :workspace => current_workspace, 
+    #                                                                         :role => manager ).as_null_object 
+    #                                  end                              
+                                 
+
+  before(:each) do
+    controller.stub(:authenticate_user!         => true                     )                 
+    #controller.stub(:current_workspace_engineer => mock_workspace_engineer  )
+    #controller.stub(:current_workspace_manager  => mock_workspace_manager   ) 
+    controller.stub(:current_workspace          => current_workspace        )   
+    @engineer_member  = FactoryGirl.create(:engineer_member                 )
+    @manager_member   = FactoryGirl.create(:manager_member                  )
+    
+    current_workspace.save
+    other_workspace.save
+  end  
         
     it "redirect to workspace/sc_models when user is an engineer" do 
-      #Workspace.should_receive(:?) 
-      get :show, :id => current_workspace.id
-      # NOTE: ne fonctionne pas response.should render_template("workspace/show").with(:layout =>'workspace')
-      response.should render_template(["layouts/workspace", "workspace/show"])      
+      get :show, :id => current_workspace.id    
+      assigns(:engineer_member ).should eq( [@engineer_member] )
+      response.should render_template("scmodels/index")      
     end
     
     it "redirect to workspace/bills when user is an manager" do 
-      #Workspace.should_receive(:?) 
-      get :show, :id => other_workspace.id
-      # NOTE: ne fonctionne pas response.should render_template("workspace/show").with(:layout =>'workspace')
-      response.should redirect_to(workspace_path(current_workspace))
-      flash[:notice].should eq("Vous demandez l'affichage d'une page appartenant à un autre espace de travail.")
+      get :show, :id => current_workspace.id           
+      assigns(:manager_member ).should eq( [@manager_member] )
+      response.should render_template("bills/index")      
     end
     
     # it "workspace can be acces if it is current_workspace" do 
