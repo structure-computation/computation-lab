53eea38f08b923002663d42994bb8bc8f459d8ae
stephanie chhim <stephaniechhim@gmail.com>
2011-09-16 19:20:25 +0200

    Suite travail Spec de HomeController

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,71 +3,73 @@ require 'spec_helper'
 describe "Where will it redirect you when you log in" do 
   let :current_workspace        do FactoryGirl.build(:workspace)                                         end
   let :other_workspace          do FactoryGirl.build(:workspace)                                         end                                     
-  let :mock_current_member      do mock_model(UserWorkspaceMembership, :workspace => current_workspace)  end
+  let :mock_workspace_member      do mock_model(UserWorkspaceMembership, :workspace => current_workspace)  end
     
   describe HomeController do
-    context "You log in as an Engineer" do   
-       
-      before(:each) do           
-        controller.stub(:authenticate_user! => true                                      ) 
-        controller.stub(:current_member     => mock_current_member                       )   
-        #@engineer_member  = FactoryGirl.create(:member, :workspace =>  current_workspace )   
-      end                       
-      
-      it "redirect to workspace/X/sc_models" do
-        UserWorkspaceMembership.stub!(:engineer).and_return(1)     
-        get :index, :id => current_workspace.id    
-        assigns(:workspace ).should eq( [@current_workspace] )
-        response.should redirect_to(workspace_sc_models_path(current_workspace)) 
-      end   
-                          
-      # Engineer n'a pas accès aux parties financières
-      context "An Engineer want to access to financial part" do
-        before (:each) do
-        # 
+    describe "Access to Laboratory" do  
+      context "Log in as an Engineer" do  
+        before(:each) do           
+          controller.stub(:authenticate_user! => true                ) 
+          controller.stub(:current_member     => mock_current_member )  
+          mock_workspace_member.stub(:engineer? => true)
+          #engineer_member  = FactoryGirl.create(:engineer_member ) 
+          #UserWorkspaceMembership.stub!(:engineer).and_return(1)     
+        end                              
+        it "redirect to workspace/X/sc_models" do
+          get :index, :id => current_workspace.id    
+          assigns(:workspace ).should eq( [@current_workspace] )
+          response.should redirect_to(workspace_sc_models_path(current_workspace)) 
+        end   
+      # enf of context log in as an engineer
+      end      
+                    
+      context "Not an Engineer" do
+        before (:each) do  
+          controller.stub(:authenticate_user! => true                 ) 
+          controller.stub(:current_member     => mock_current_member  ) 
+          mock_workspace_member.stub(:engineer? => false              )  
         end
         it "should not redirect to /workspaces/X#Factures" do     
           get :index, :id => current_workspace.id              
-          UserWorkspaceMembership.stub!(:engineer).and_return(1) 
           response.should redirect_to(workspace_sc_models_path(current_workspace))  
-          #response.should_not be_redirect
           flash[:notice].should eq("Vous n'avez pas accès à cette partie de l'espace de travail.")
         end
-      #end of forbidden financiel part           
+      #end of context not an engineer         
       end
-    #end of context Engineer 
+    #end of describe access to laboratory 
     end                 
     
-    context"You log in as a Manager" do      
-      before(:each) do  
-        controller.stub(:authenticate_user! => true                                     ) 
-        controller.stub(:current_member     => mock_current_member                      ) 
-        #@manager_member  = FactoryGirl.create(:member, :workspace =>  current_workspace )
-      end                            
-      it "redirect to /workspaces/X#Factures" do
-        UserWorkspaceMembership.stub!(:manager).and_return(1)  
-        get :index, :id => current_workspace.id    
-        assigns(:workspace ).should eq( [@current_workspace] )
-        response.should redirect_to(workspace_bills_path(current_workspace))  
-      end   
+    describe "Access to Finance" do  
+      context "Log in as a Manager" do
+        before(:each) do  
+          controller.stub(:authenticate_user! => true                ) 
+          controller.stub(:current_member     => mock_current_member )
+          mock_workspace_member.stub(:manager? => true               )
+           
+        end                            
+        it "redirect to /workspaces/X#Factures" do
+          get :index, :id => current_workspace.id    
+          assigns(:workspace ).should eq( [@current_workspace] )
+          response.should redirect_to(workspace__path(current_workspace))  
+        end  
+      #end of context log in as a manager   
+      end
       
-      # Manager n'a pas accès aux parties laboratoires    
-      context "An Manager want to acces to laboratory part" do   
+      context "Not a Manager" do
         before (:each) do    
-        #  
+          controller.stub(:authenticate_user! => true                ) 
+          controller.stub(:current_member     => mock_current_member ) 
+          mock_workspace_member.stub(:manager? => false              )
         end                          
-        
         it"should not redirect to workspace/X/sc_models"do
-          UserWorkspaceMembership.stub!(:manager).and_return(1)      
           get :index, :id => current_workspace.id 
-          #response.should_not be_redirect   
           response.should redirect_to(workspace_bills_path(current_workspace))
           flash[:notice].should eq("Vous n'avez pas accès à cette partie de l'espace de travail.")
         end  
-      #end of forbidden laboratory part  
+      #end of context not a manager  
       end 
-    #end of context Manager
+    #end of describe access to finance
     end 
-   #end of describe HomeController 
- end 
+  #end of describe HomeController 
+  end 
 end
