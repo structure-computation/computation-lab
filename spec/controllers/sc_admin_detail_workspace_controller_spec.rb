09caf0ab0b5db02791c4112982b12ca60aedc505
stephanie chhim <stephaniechhim@gmail.com>
2011-09-20 15:47:43 +0200

    Annule test ScAdminDetailWorkspaceController

--- spec/controllers/sc_admin_detail_workspace_controller_spec.rb
+++ spec/controllers/sc_admin_detail_workspace_controller_spec.rb
@@ -1,1 +1,34 @@
-require 'spec_helper'
+require 'spec_helper' 
+
+describe ScAdminDetailWorkspaceController do 
+  let :mock_forfait          do mock_model(Forfait).as_null_object                 end  
+  let :mock_abonnement       do mock_model(Abonnement).as_null_object             end
+  let :mock_workspace_member do mock_model(UserWorkspaceMembership).as_null_object end 
+  
+  before(:each) do
+    controller.stub(:authenticate_user!       =>  true                  ) 
+    @forfait     = FactoryGirl.create(:forfait                          )                                
+    @abonnement  = FactoryGirl.create(:abonnement                       )
+    @forfait.save 
+    @abonnement.save      
+  end
+
+  describe "Access for managers roles" do
+    before(:each) do mock_workspace_member.stub(:manager? => true) end
+    context "When accessing details : forfaits and abonnements" do     
+      it "can access index" do get :index  ;                          should respond_with(:success)   end
+      it "can access show"  do get :show   ,  :id => @forfait.id ;    should respond_with(:success)   end
+      it "can access show"  do get :show   ,  :id => @abonnement.id ; should respond_with(:success)   end
+                                             
+    end   
+  end     
+  
+  describe "Acces for non-managers roles" do
+    before(:each) do mock_workspace_member.stub(:manager? => false) end
+    context "When accessing to details" do
+      it "can NOT access index" do get :index ;                         should respond_with(:forbidden) end 
+      it "can NOT access show"  do get :show  , :id => @forfait.id ;    should respond_with(:forbidden) end   
+      it "can NOT access show"  do get :show  , :id => @abonnement.id ; should respond_with(:forbidden) end   
+    end                                        
+  end                                          
+end
