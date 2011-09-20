c854e6a9a80d5ef49394a57ac98fcffda21bd181
stephanie chhim <stephaniechhim@gmail.com>
2011-09-20 14:34:30 +0200

    Test De HomeController OK Ajout du cas "user is an engineer and a manager" , redirecion à la vue de index de ScModels lors du logging Restructuration des specs du test de HomeControlle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,14 +10,23 @@ class HomeController < ApplicationController
   
   def index     
     case 
-    when current_workspace_member.engineer?   
-      redirect_to workspace_sc_models_path(current_workspace_member.workspace)
-    when current_workspace_member.manager? 
-      redirect_to workspace_path(current_workspace_member.workspace)    
+    when  current_workspace_member.engineer? && !current_workspace_member.manager?   
+      redirect_to workspace_sc_models_path(current_workspace_member.workspace)        
+      
+    when   current_workspace_member.manager? && !current_workspace_member.engineer? 
+      redirect_to workspace_path(current_workspace_member.workspace)                
+      
+    when  current_workspace_member.engineer? && current_workspace_member.manager?  
+      redirect_to workspace_sc_models_path(current_workspace_member.workspace)        
+        
     when !current_workspace_member.engineer? && !current_workspace_member.manager?  
-      render :index
-      flash[:notice] = "Vous n'avez pas de statut (ingénieur ou gestionnaire) attribué. Veuillez contacter le service client." 
-    end                                                                                 
+      #render :index
+      respond_to do |format|
+      format.html {redirect_to workspace_path(current_workspace_member.workspace), 
+                  :notice => "Vous n'avez pas de statut (ingénieur ou gestionnaire) attribué. Veuillez contacter le service client."}
+      format.json {render :status => 404, :json => {}}   
+      end   
+    end                                                                              
   #end of index  
   end
 end
