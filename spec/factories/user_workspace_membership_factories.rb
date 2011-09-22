85d9abd3650b2fb97387a4322945f1e7e77c83b4
Raphaël Bellec <contact@bellec.in>
2011-09-23 00:50:46 +0200

    Création dans les factories d'un workspace complet avec 2 utilisateurs. Pour les tests d'intégration. Ce workspace sera ammené à évoluer pour integrer un modèle, des materiaux, etc.

--- spec/factories/user_workspace_membership_factories.rb
+++ spec/factories/user_workspace_membership_factories.rb
@@ -16,7 +16,29 @@ FactoryGirl.define do
     end
     factory :not_manager_member do
       manager 0
-    end                  
+    end        
+    
+    # Ces membres sont des utilisateurs avec un mail et un mdp (fixé par défaut dans la factory user) destiné à être utilisés pour les tests de
+    # login
+    factory :test_manager do
+      engineer 0
+      manager  1
+      association :user, :factory => :test_user, :email => 'manager@test.com'
+    end
+    
+    factory :test_engineer do
+      engineer 1
+      manager  0
+      association :user, :factory => :test_user, :email => 'engineer@test.com'
+    end
+    
+    factory :test_manager_engineer do
+      engineer 1
+      manager  1
+      association :user, :factory => :test_user, :email => 'manager_engineer@test.com'
+    end
+    
+              
   end
   
 end
