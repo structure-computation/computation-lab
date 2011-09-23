d5e2c05b375386aac7d1c44dbed1c8d9735e2d5f
stephanie chhim <stephaniechhim@gmail.com>
2011-09-23 17:13:37 +0200

    Modification de la méthode ajouter un membre à la liste de WorkspaceMemberToModelOwnership

--- app/models/physical/workspace_member_to_model_ownership.rb
+++ app/models/physical/workspace_member_to_model_ownership.rb
@@ -2,11 +2,12 @@ class WorkspaceMemberToModelOwnership < ActiveRecord::Base
   belongs_to  :workspace_member, :class_name => "UserWorkspaceMembership", :foreign_key => "workspace_member_id"
   belongs_to  :sc_model # TODO: spécifier un chargement automatique (:includes => true ?)   
   
-  def addWorkspaceMemberToModelOwnership
-       @workspace_members = UserWorkspaceMembership.find(current_workspace_member)
-       @sc_model = ScModel.find(params[:id])
-       @sc_model.workspace_members << @workspace_member
-       flash[:notice] = 'Sc_model was shared.'
+  #On ajoute un membre de l'espace de travail et on lui donne les droits de lire ce modèle
+  def addWorkspaceMemberToModelOwnership 
+    @member_to_add = UserWorkspaceMembership.find_by_id([:id])
+    @sc_model = ScModel.find(params[:id])
+    @sc_model.workspace_members << @member_to_add(:rights => "readonly") 
+    flash[:notice] = 'Sc_model was shared.'     
    end   
             
 end
