d4abcb2e8a550f16e19728661204c46407e7a0b7
j.bellec <j.bellec@structure-computation.com>
2011-12-22 10:12:24 +0100

    ajout de l interface Computation-lab dans ext_app et des lien vers Soja...

--- app/controllers/ext_app_visu_controller.rb
+++ app/controllers/ext_app_visu_controller.rb
@@ -1,6 +1,7 @@
 class ExtAppVisuController < ApplicationController
   layout "application_ext_app"
   def index
-    @page = :home # Pour afficher le menu en selected.
+    #@page = :home # Pour afficher le menu en selected.
+    render :layout => false 
   end
 end
