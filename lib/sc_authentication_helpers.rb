1fd2bbf12a83e531357b30f7461b58a534484b5d
Raphaël Bellec <contact@bellec.in>
2012-04-20 18:27:00 +0200

    Correction de la définition de la lib d'auth en ajoutant un module.

---
+++ lib/sc_authentication_helpers.rb
@@ -0,0 +1,33 @@
+
+module  SCAuthenticationHelpers
+  
+  # TODO: fait doublon avec la même procédure dans ApplicationController. 
+  # Trouver la "bonne methode".
+  def current_workspace_member
+    # Si déjà initialisé, on renvoie l'objet existant.
+    # Sinon on le recherche.
+    @current_workspace_member = UserWorkspaceMembership.find(session[:current_workspace_member_id]) unless @current_workspace_member
+    @current_workspace_member 
+  end
+
+  # Change le current_workspace_member.
+  # TODO : Check que l'on est sur le même user ? Check de droits ?
+  def   current_workspace_member=           new_current_workspace_member
+    session[:current_workspace_member_id] = new_current_workspace_member.id
+    @current_workspace_member             = new_current_workspace_member
+  end
+
+  # Access control. Use as before_filter on actions that require the workspace member to be an engineer 
+  def must_be_engineer
+    if ! current_workspace_member.engineer?
+      render  "static/forbidden", :status => :forbidden 
+    end
+  end    
+
+  # Access control. Use as before_filter on actions that require the workspace member to be an manager 
+  def must_be_manager
+    if ! current_workspace_member.manager?
+      render  "static/forbidden", :status => :forbidden 
+    end
+  end
+end
