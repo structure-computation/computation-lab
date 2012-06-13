bc895e89734664586188bfc984337abfdb7f930c
j.bellec <j.bellec@structure-computation.com>
2012-06-13 09:55:33 +0200

    correction : dans le controller de scratch user, on a pas besoin d etre identifier pour creer un nouvel utilisateur

--- app/controllers/scratch_user_controller.rb
+++ app/controllers/scratch_user_controller.rb
@@ -1,5 +1,5 @@
 class ScratchUserController < InheritedResources::Base  
-  before_filter :authenticate_user!  
+  before_filter :authenticate_user!  , :except => [:new, :create]   
   layout 'login'
   
   def index
