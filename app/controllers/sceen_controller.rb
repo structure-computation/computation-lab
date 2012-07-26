c973117fb09d054a6715a149ba4ff51a062af40f
Jeremie Bellec <j.bellec@structure-computation.com>
2012-07-26 18:19:22 +0200

    test pour la prise en compte du numero de modele avec Soja

--- app/controllers/sceen_controller.rb
+++ app/controllers/sceen_controller.rb
@@ -5,6 +5,7 @@ class SceenController < ApplicationController
   def index
     @current_model = current_workspace_member.sc_models.find(params[:sc_model_id])
     @current_model.tool_in_use("sceen", current_workspace_member)
+    @model_id = params[:sc_model_id]
     #@page = :home # Pour afficher le menu en selected.
     render :layout => false 
   end
@@ -32,7 +33,7 @@ class SceenController < ApplicationController
 #     
 #   end
   
-  def _
+  def upload
     #debugger
     #logger.debug params
     #request = params[:data] + "$"
@@ -45,12 +46,39 @@ class SceenController < ApplicationController
     port = 8888                           # Default HTTP port
     #logger.debug "avant open _"
     socket = TCPSocket.open(host,port)  # Connect to server
-    #logger.debug  request.method + " " + request.fullpath + " \n\n" + request.raw_post 
+    logger.debug  request.method + " " + request.fullpath + " \n\n" + request.raw_post 
     #socket.print( "POST /sceen/_ \n\n" + request.raw_post ) # Send request
     #logger.debug "avant print  _"
     socket.write( request.method + " " + request.fullpath + " \n\n" + request.raw_post ) # Send request
     #debugger
     #logger.debug "avant read _"
+    response = socket.read              # Read complete response
+    socket.close
+    #logger.debug response
+    #logger.debug "après read _"
+    #     response1 = response.split(/\n\n/).drop(1).join("\n\n")
+    #logger.debug response
+    render :text => response
+  end
+  
+  
+  def _
+    #debugger
+    #logger.debug params
+    #request = params[:data] + "$"
+    #logger.debug request
+    
+    #@current_model = current_workspace_member.sc_models.find(params[:sc_model_id])
+    #@current_model.tool_in_use("sceen", current_workspace_member)
+    
+    host = 'localhost'     # The web server
+    port = 8888                           # Default HTTP port
+    socket = TCPSocket.open(host,port)  # Connect to server
+    #logger.debug  request.method + " " + request.fullpath + " \n\n" + request.raw_post 
+    length = request.raw_post.length
+    logger.debug  "request.raw_post.length = " + length.to_s
+    #logger.debug  request.method + " " + request.fullpath + " Content-Length: " + length.to_s + " \n\n" + request.raw_post
+    socket.write( request.method + " " + request.fullpath + " Content-Length: " + length.to_s + " \n\n" + request.raw_post ) # Send request
     response = socket.read              # Read complete response
     socket.close
     #logger.debug response
