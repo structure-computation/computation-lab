5e5af5761563597873edda6658840baac5b0f067
scproduction_sc2 <contact@structure-computation.com>
2013-03-04 11:23:08 +0100

    petite modifs locales

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ SCInterface::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors  = false
-  config.action_mailer.default_url_options    = { :host => 'localhost:3000' } 
+  config.action_mailer.default_url_options    = { :host => 'beta.computation-lab.com' } 
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
