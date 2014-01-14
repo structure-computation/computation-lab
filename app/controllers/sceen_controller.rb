1e35254ed5d70e874c7d0ef0dc2a07e1282aed8d
Jeremie Bellec <j.bellec@structure-computation.com>
2014-01-14 15:30:52 +0100

    ajout des fonctionnalité pour IpolLabs et de pool = 50 dans database.yml

--- app/controllers/sceen_controller.rb
+++ app/controllers/sceen_controller.rb
@@ -72,8 +72,12 @@ class SceenController < ApplicationController
     #@current_model.tool_in_use("sceen", current_workspace_member)
     
     #host = 'localhost'     # The web server
-    host = '192.168.38.241'
-    port = 8888                           # Default HTTP port
+    #host = '192.168.38.241'
+    #port = 8888                           # Default HTTP port
+    
+    host = '91.121.167.175'     # The web server
+    port = 80                           # Default HTTP port
+    
     socket = TCPSocket.open(host,port)  # Connect to server
     #logger.debug  "request.method = " + request.method + " " + request.fullpath + " \n\n" + request.raw_post 
     length = request.raw_post.length
